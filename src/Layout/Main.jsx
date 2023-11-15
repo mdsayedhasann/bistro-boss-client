38c5bec6af341f34867c3384a5ea49e935ba294c
sayed_hossain <sayed.hasan0011@gmail.com>
2023-11-15 12:14:20 +0600

    Header and Footer added on Main.jsx

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '../pages/shared/Header/Header';
+import Footer from '../pages/shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
+            <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
