b307f53cefc3ac3a9185b2258abab6e863c4370e
sayed_hossain <sayed.hasan0011@gmail.com>
2023-11-15 11:52:04 +0600

    Router setting up run put Home.jsx

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,10 +2,18 @@ import {
     createBrowserRouter
   } from "react-router-dom";
 import App from "../App";
+import Main from "../Layout/Main";
+import Home from "../pages/Home/Home/Home";
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <App></App>,
+      element: <Main></Main>,
+      children: [
+          {
+              path: '/',
+              element: <Home></Home>
+          }
+      ]
     },
   ]);
