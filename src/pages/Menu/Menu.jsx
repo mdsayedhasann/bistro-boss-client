9f9c2882445504c2c88a17498e7fbdb71fb75d17
sayed_hossain <sayed.hasan0011@gmail.com>
2023-11-17 01:34:03 +0600

    using background image Parallax from React Parallax

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import Cover from "../shared/Cover/Cover";
 import img from "../../assets/menu/banner3.jpg";
+import PopularMenu from "../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
   return (
@@ -16,7 +17,32 @@ const Menu = () => {
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
-      <h1>I am Menu</h1>
+
+      <PopularMenu></PopularMenu>
+      <Cover
+        img={img}
+        heading={"OUR MENU"}
+        content={
+          "Would you like to try a dish?."
+        }
+      ></Cover>
+      <Helmet>
+        <title>Bistro Boss | Menu</title>
+      </Helmet>
+
+      <PopularMenu></PopularMenu>
+      <Cover
+        img={img}
+        heading={"OUR MENU"}
+        content={
+          "Would you like to try a dish?."
+        }
+      ></Cover>
+      <Helmet>
+        <title>Bistro Boss | Menu</title>
+      </Helmet>
+
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
--- src/pages/shared/Cover/Cover.jsx
+++ src/pages/shared/Cover/Cover.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { Parallax } from "react-parallax";
 
 const Cover = ({ img, heading, content }) => {
   return (
-    <div style={{backgroundImage: `url(${img})`}} className="bg-cover py-36 bg-no-repeat">
-      <div className="bg-black bg-opacity-60 py-20 w-3/4 mx-auto flex justify-center items-center text-white flex-col px-10">
-        <h3 className="text-3xl py-3">{heading}</h3>
-        <p>
-         {
-             content
-         }
-        </p>
+    <>
+    <Parallax
+      blur={{ min: -15, max: 15 }}
+      bgImage={img}
+      bgImageAlt="the dog"
+      strength={-200}
+    >
+      <div
+        className="bg-cover py-36 bg-no-repeat"
+      >
+        <div className="bg-black bg-opacity-60 py-20 w-3/4 mx-auto flex justify-center items-center text-white flex-col px-10">
+          <h3 className="text-3xl py-3">{heading}</h3>
+          <p>{content}</p>
+        </div>
       </div>
-    </div>
+    </Parallax>
+    </>
   );
 };
 
