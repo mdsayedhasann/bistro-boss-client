4638e7df0e5f8ebda55ca7cd85ebb992fffd16ed
sayed_hossain <sayed.hasan0011@gmail.com>
2023-11-18 01:24:34 +0600

    Deep dynamic

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -5,15 +5,18 @@ import img from "../../assets/menu/banner3.jpg";
 import PopularMenu from "../Home/PopularMenu/PopularMenu";
 import useMenu from "../../hooks/useMenu";
 import MenuItem from "../shared/MenuItem/MenuItem";
+import SectionHeading from "../../components/SectionHeading";
+import MenuCategory from "./MenuCategory/MenuCategory";
+import pizzaBg from '../../assets/menu/pizza-bg.jpg'
 
 const Menu = () => {
-  const [menu] = useMenu()
-  const dessert = menu.filter(item => item.category === 'dessert')
-  const pizza = menu.filter(item => item.category === 'pizza')
-  const salad = menu.filter(item => item.category === 'salad')
-  const soup = menu.filter(item => item.category === 'soup')
-  const offered = menu.filter(item => item.category === 'offered')
-  const drinks = menu.filter(item => item.category === 'drinks')
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const salad = menu.filter((item) => item.category === "salad");
+  const soup = menu.filter((item) => item.category === "soup");
+  const offered = menu.filter((item) => item.category === "offered");
+  const drinks = menu.filter((item) => item.category === "drinks");
   return (
     <div>
       <Helmet>
@@ -22,33 +25,27 @@ const Menu = () => {
       <Cover
         img={img}
         heading={"OUR MENU"}
-        content={
-          "Would you like to try a dish?."
-        }
+        content={"Would you like to try a dish?."}
       ></Cover>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 my-10'>
-                {
-                dessert.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
-                
-                }
-      </div>
+      <SectionHeading
+        subheading={"Don't Miss Today's offer"}
+        heading={"Our Special Desert"}
+      ></SectionHeading>
+    <MenuCategory items={dessert}></MenuCategory>
 
       {/* Pizza */}
       <Cover
         img={img}
         heading={"OUR Pizza"}
-        content={
-          "Would you like to try a dish?."
-        }
+        content={"Would you like to try a dish?."}
       ></Cover>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 my-10'>
-                {
-                pizza.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
-                
-                }
-      </div>
+      <SectionHeading
+        subheading={"Don't Miss Today's offer"}
+        heading={"Our Special Pizzas"}
+      ></SectionHeading>
 
-
+      {/* Pizza Again */}
+      <MenuCategory items={pizza} title={'Special Pizza using critical'} coverImage={pizzaBg}></MenuCategory>
     </div>
   );
 };
--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import Cover from "../../shared/Cover/Cover";
+import MenuItem from "../../shared/MenuItem/MenuItem";
 
-const MenuCategory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const MenuCategory = ({ items, title, coverImage, content }) => {
+  return (
+    <div>
+      {/* Cover Start */}
+      { title &&
+        <Cover
+          img={coverImage}
+          heading={title}
+          content={content}
+        ></Cover>
+      }
+      {/* Cover End */}
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-10">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MenuCategory;
