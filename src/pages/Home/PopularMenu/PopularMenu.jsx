b5f89e70c77790a2c6c1f2d2d72ff3d6b5ebfa46
sayed_hossain <sayed.hasan0011@gmail.com>
2023-11-18 01:00:32 +0600

    Used Custom hooks useMenu and implement on Menu.jsx file and PopolarMenu.jsx

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect, useState } from 'react';
 import SectionHeading from '../../../components/SectionHeading';
 import MenuItem from '../../shared/MenuItem/MenuItem';
+import useMenu from '../../../hooks/useMenu';
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([])
-    useEffect(() => {
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const popularItems = data.filter(item => item.category === 'popular')
-            setMenu(popularItems)
-        })
-    } ,[])
+    const [menu] = useMenu()
+    const popular = menu.filter(item => item.category === 'popular')
+    // const [menu, setMenu] = useState([])
+    // useEffect(() => {
+    //     fetch('menu.json')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         const popularItems = data.filter(item => item.category === 'popular')
+    //         setMenu(popularItems)
+    //     })
+    // } ,[])
     return (
         <section className='py-5'>
             <SectionHeading subheading={'Check it Out'} heading={'From Our Menu'}>
@@ -19,7 +21,7 @@ const PopularMenu = () => {
             </SectionHeading>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
                 {
-                menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+                popular.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
                 
                 }
             </div>
--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -3,10 +3,22 @@ import { Helmet } from "react-helmet-async";
 import Cover from "../shared/Cover/Cover";
 import img from "../../assets/menu/banner3.jpg";
 import PopularMenu from "../Home/PopularMenu/PopularMenu";
+import useMenu from "../../hooks/useMenu";
+import MenuItem from "../shared/MenuItem/MenuItem";
 
 const Menu = () => {
+  const [menu] = useMenu()
+  const dessert = menu.filter(item => item.category === 'dessert')
+  const pizza = menu.filter(item => item.category === 'pizza')
+  const salad = menu.filter(item => item.category === 'salad')
+  const soup = menu.filter(item => item.category === 'soup')
+  const offered = menu.filter(item => item.category === 'offered')
+  const drinks = menu.filter(item => item.category === 'drinks')
   return (
     <div>
+      <Helmet>
+        <title>Bistro Boss | Menu</title>
+      </Helmet>
       <Cover
         img={img}
         heading={"OUR MENU"}
@@ -14,35 +26,29 @@ const Menu = () => {
           "Would you like to try a dish?."
         }
       ></Cover>
-      <Helmet>
-        <title>Bistro Boss | Menu</title>
-      </Helmet>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 my-10'>
+                {
+                dessert.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+                
+                }
+      </div>
 
-      <PopularMenu></PopularMenu>
+      {/* Pizza */}
       <Cover
         img={img}
-        heading={"OUR MENU"}
+        heading={"OUR Pizza"}
         content={
           "Would you like to try a dish?."
         }
       ></Cover>
-      <Helmet>
-        <title>Bistro Boss | Menu</title>
-      </Helmet>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 my-10'>
+                {
+                pizza.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+                
+                }
+      </div>
 
-      <PopularMenu></PopularMenu>
-      <Cover
-        img={img}
-        heading={"OUR MENU"}
-        content={
-          "Would you like to try a dish?."
-        }
-      ></Cover>
-      <Helmet>
-        <title>Bistro Boss | Menu</title>
-      </Helmet>
 
-      <PopularMenu></PopularMenu>
     </div>
   );
 };
