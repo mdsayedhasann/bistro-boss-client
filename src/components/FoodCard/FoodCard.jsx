5b39023c70c244e4dbba8de0db219d6a13a28e97
sayed_hossain <sayed.hasan0011@gmail.com>
2023-11-24 00:55:55 +0600

    non logged in users are getting a error message when trying to add a product to their cart

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+import useAuth from "../../hooks/useAuth";
+import Swal from 'sweetalert2'
 
 const FoodCard = ({ item }) => {
   const { name, image, recipe, price } = item;
+  // const {user} = useContext(AuthContext)
+  const {user} = useAuth()
+
   const handleAddToCart = (food) => {
     console.log(food);
+    if(user && user.email){
+      // Send Item to the database
+    }
+    else{
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "You must log in first before adding any product to your cart",
+        footer: '<a href="#">Why do I have this issue?</a>'
+      });
+    }
   }
   return (
     <div className="card bg-base-100 shadow-xl">
